13fb3da17271ea39ada832ef4e2ce3aed93d84e4
lcsanc <47239003+lcsanc@users.noreply.github.com>
2022-03-31 20:48:27 -0500

    Add files via upload

---
+++ FirstBank/src/Account.java
@@ -0,0 +1,120 @@
+import java.util.Scanner; 
+
+public class Account 
+{
+	int balance;
+	int previousTransaction;
+	String customerName;
+	String customerID;
+	
+	Account(String cname, String cid)
+	{
+		customerName = cname; 
+		customerID = cid; 
+	}
+	
+	void deposit(int amount)
+	{
+		balance = balance + amount;
+		previousTransaction = amount;
+	}
+	
+	void withdraw(int amount)
+	{
+		if (amount!=0)
+		{
+			balance = balance - amount;
+			previousTransaction = -amount;
+		}
+	}
+	
+	void getPreviousTransaction()
+	{
+		if(previousTransaction > 0)
+		{
+			System.out.println("Deposited: " + previousTransaction);
+		}
+		else if (previousTransaction < 0)
+		{
+			System.out.println("Withdrwan: " + Math.abs(previousTransaction));
+		}
+		else
+		{
+			System.out.println("No transaction Occurred");
+		}
+	}
+	
+	void calculateInterest(int years)
+	{
+		double interestRate = .0185;
+		double newBalance = (balance * interestRate * years) + balance;
+		System.out.println("The current interest rate is " + (100 * interestRate) + "%");
+		System.out.println("After " + years + " years, you balance will be: " + newBalance);
+	}
+	void showMenu()
+	{
+		char option = '\0';
+		Scanner scanner = new Scanner (System.in);
+		System.out.println("Welcome, " + customerName + "!");
+		System.out.println("Your ID is: " + customerID);
+		System.out.println();
+		System.out.println("What would you like to do?");
+		System.out.println();
+		System.out.println("A. Check your balance");
+		System.out.println("B. Make a deposit");
+		System.out.println("C. Make a withdrawal");
+		System.out.println("D. View previous trasaction");
+		System.out.println("E. Calculate interest");
+		System.out.println("F. Exit");
+		
+		do
+		{
+			System.out.println();
+			System.out.println("Enter an option: ");
+			char option1 = scanner.next().charAt(0);
+			option = Character.toUpperCase(option1);
+			System.out.println();
+			
+			switch(option)
+			{
+			case 'A':
+				System.out.println("==================================");
+				System.out.println("Balance = $" + balance);
+				System.out.println("==================================");
+				System.out.println();
+				break;
+			case 'B':
+				System.out.println("Enter amount to deposit: ");
+				int amount = scanner.nextInt();
+				deposit(amount);
+				System.out.println();
+				break;
+			case 'C':
+				System.out.println("Enter an amount to withdraw: ");
+				int amount2 = scanner.nextInt();
+				withdraw(amount2);
+				System.out.println();
+				break;
+			case 'D':
+				System.out.println("==================================");
+				getPreviousTransaction();
+				System.out.println("==================================");
+				System.out.println();
+				break;
+			case 'E':
+				System.out.println("Enter how many years of accrued interest: ");
+				int years = scanner.nextInt();
+				calculateInterest(years);
+				break;
+			case 'F':
+				System.out.println("==================================");
+				break;
+				
+				default:
+					System.out.println("Error: invalid option. Please enter A, B, C, D, E, F");
+					break;
+			}
+		} while (option != 'F');
+		System.out.println("Thank you for banking with us!"); 
+	}
+}
---
+++ FirstBank/src/FirstBank.java
@@ -0,0 +1,12 @@
+
+public class FirstBank 
+{
+	public static void main(String[] args)
+	{
+		Account tim = new Account("Tim Statler","A00001");
+
+		
+		Account beyonce = new Account("Beyonce", "A00002");
+		beyonce.showMenu();
+	}
+}
